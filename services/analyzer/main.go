7d864061f3bbeda7d840a1774bb5a64bf7d3b4e1
Joseph Ward <joward@adobe.com>
2019-02-07 00:19:44 -0800

    Removing main.go

--- services/analyzer/main.go
+++ services/analyzer/main.go
@@ -13,32 +13,6 @@ type Config struct {
 func main() {
 	config := loadConfig()
 
-	err := loadModel()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	r := gin.Default()
-
-	r.GET("/labels", func(c *gin.Context) {
-		url := c.Query("url")
-		result, err := classifyImage(url)
-		if err != nil {
-			_ = c.AbortWithError(500, err)
-		} else {
-			c.JSON(200, result)
-		}
-	})
-
-	err = r.Run("0.0.0.0:" + config.Port) // listen and serve
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func _main() {
-	config := loadConfig()
-
 	r := gin.Default()
 
 	r.GET("/labels", func(c *gin.Context) {
